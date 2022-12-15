7bd37edf093cbbb19164b28e0bd63830e371fb0a
guygubaby <guygubaby@gmail.com>
2022-12-15 21:41:54 +0800

    chore: update deps and remove useless deps

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,5 @@
 import { defineConfig } from 'vitest/config'
-import AutoImport from 'unplugin-auto-import/vite'
 
 export default defineConfig({
-  test: {
-    globals: true,
-  },
-  plugins: [
-    AutoImport({
-      imports: ['vitest'],
-      dts: true,
-    }),
-  ],
+  test: { },
 })
